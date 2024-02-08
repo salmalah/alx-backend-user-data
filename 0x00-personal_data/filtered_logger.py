ca60c18f873083cf9846a628757913012e6180c3
salmalah <122815714+salmalah@users.noreply.github.com>
2024-02-08 21:27:34 +0100

    Update filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,6 +6,7 @@ This module to define filtered_logger
 
 import re
 from typing import List
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
@@ -17,3 +18,23 @@ def filter_datum(fields: List[str], redaction: str, message: str,
         message = re.sub(rf"{i}=(.*?)\{separator}",
                          f'{i}={redaction}{separator}', message)
     return message
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: list):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        return re.sub(
+            r'(?<=\b(?:' + '|'.join(self.fields) + r')=)[^{}]*(?=[{}])'.format(
+                self.SEPARATOR, self.SEPARATOR
+            ),
+            self.REDACTION,
+            record.getMessage(),
+        )
